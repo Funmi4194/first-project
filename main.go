48155cd579e1fc77c7de7ce4fb61f319c4d5a8d1
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2022-09-02 12:05:10 +0100

    modified code

--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
-func getTarget(number[]int, target int)[]int{
-	var result := []int{}
+import "fmt"
 
-	for i:=0; i<len(number); i++{
-		for j:= i+1; j<len(number); j++{
-			sum := number[i] +number[j]
-
-			if sum == target{
-				result = append(result, number[i])
-				result = append(result, number[j])
-				return result
-			}
-		}
+func status(name string) {
+	grades := map[string]int{"Alma": 0, "Robert": 66, "Funmi": 78, "Precious": 46}
+	if grade, ok := grades[name]; !ok {
+		fmt.Printf("%q has not been assigned a score yet\n", name)
+	} else if grade < 60{
+		fmt.Printf("%q with %v marks has failed, Kindly retake course!\n", name, grades[name])
+	}else{
+		fmt.Printf("%q has passed with %v marks\n", name, grades[name])
 	}
-	return result
 }
 
 func main(){
+	status("Alma")
+	status("Funmi")
+	status("Damilola")
+	status("Precious")
+	status("Robert")
 
 }
+
+//// A code to find a given target number
+// package main
+// import "fmt"
+// func getTarget(number[]int, target int)[]int{
+// 	var result = []int{}
+
+// 	for i:=0; i<len(number); i++{
+// 		for j:= i+1; j<len(number); j++{
+// 			sum := number[i] +number[j]
+
+// 			if sum == target{
+// 				result = append(result, number[i])
+// 				result = append(result, number[j])
+// 				return result
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
+
+// func main(){
+// 	fmt.Println(getTarget([]int{2, 7, 6, 8, 5}, 9))
+
+// }
